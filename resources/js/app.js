5b57ca4c1d29565d8c9b9e7b658a36d3f1abcd2a
Rembrant Pasardan <160525899+BrantySis@users.noreply.github.com>
2024-11-08 15:14:19 +0800

    Activity 3 - Add student, install toast and spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
-
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
 import 'preline';
 
 //import Alpine from 'alpinejs';
@@ -7,3 +8,30 @@ import 'preline';
 //window.Alpine = Alpine;
 
 //Alpine.start();
+
+const notyf = new Notyf();  // Create an instance of Notyf
+
+// Function to show the toast
+function showToast(message, type = 'success') {
+    notyf.open({
+        type: type,      // 'success' or 'error'
+        message: message // The toast message to display
+    });
+}
+
+document.addEventListener('livewire:load', function () {
+    // Listen for a custom Livewire event (show-toast)
+    window.addEventListener('show-toast', event => {
+        showToast(event.detail.message);  // Show the toast with the message from Livewire
+    });
+});
+
+if (
+    localStorage.getItem("hs_theme") === "dark" ||
+    (!localStorage.getItem("hs_theme") &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    document.documentElement.classList.add("dark");
+} else {
+    document.documentElement.classList.remove("dark");
+}
